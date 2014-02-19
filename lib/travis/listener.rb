453d0c9c2de97dd311b3a9b7b39407a8719d3136
Ubuntu <ubuntu@ip-10-172-241-178.(none)>
2014-02-19 19:23:07 +0000

    dont use a redis namespace
    bin/build and the sidekiq daemon don't use the namespace.  Until I
    figure how to configure a namespace, don't use one

--- lib/travis/listener.rb
+++ lib/travis/listener.rb
@@ -20,7 +20,7 @@ module Travis
     class << self
       def setup
         ::Sidekiq.configure_client do |config|
-          config.redis = Travis.config.redis.merge(size: 1, namespace: 'sidekiq')
+          config.redis = Travis.config.redis.merge(size: 1)
         end
 
         ::Raven.configure do |config|
